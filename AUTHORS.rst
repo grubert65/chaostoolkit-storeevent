9652462c08e730f9c439e082525773a4df68263c
Masetti, Marco <marco.masetti@sky.uk>
2019-03-08 16:59:26 +0000

    based on cookiecutter

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Marco Masetti <marco.masetti@sky.uk>
+
+Contributors
+------------
+
+None yet. Why not be the first?
--- setup.py
+++ setup.py
@@ -1,77 +1,48 @@
 #!/usr/bin/env python
-import os
-import sys
-import io
+# -*- coding: utf-8 -*-
 
-import setuptools
+"""The setup script."""
 
-def get_version_from_package() -> str:
-    """
-    Read the package version from the source without importing it.
-    """
-    path = os.path.join(os.path.dirname(__file__), "chaosdb/__init__.py")
-    path = os.path.normpath(os.path.abspath(path))
-    with open(path) as f:
-        for line in f:
-            if line.startswith("__version__"):
-                token, version = line.split(" = ", 1)
-                version = version.replace("'", "").strip()
-                return version
+from setuptools import setup, find_packages
 
-name = 'chaosdb'
-desc = 'Chaos Toolkit controls lib to trace events'
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-with io.open('README.md', encoding='utf-8') as strm:
-    long_desc = strm.read()
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
 
-classifiers = [
-    'Development Status :: 2 - Pre-Alpha',  
-    'Intended Audience :: Developers',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-]
-author = 'Marco Masetti'
-author_email = 'marco.masetti@sky.uk'
-url = 'http://chaostoolkit.org'
-license = 'Apache License Version 2.0'
-packages = [
-    'chaosdb'
-]
+requirements = [ ]
 
-needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
-pytest_runner = ['pytest_runner'] if needs_pytest else []
-test_require = []
-with io.open('requirements-dev.txt') as f:
-    test_require = [l.strip() for l in f if not l.startswith('#')]
+setup_requirements = ['pytest-runner', ]
 
-install_require = []
-with io.open('requirements.txt') as f:
-    install_require = [l.strip() for l in f if not l.startswith('#')]
+test_requirements = ['pytest', ]
 
-setup_params = dict(
-    name=name,
-    version=get_version_from_package(),
-    description=desc,
-    long_description=long_desc,
-    classifiers=classifiers,
-    author=author,
-    author_email=author_email,
-    url=url,
-    license=license,
-    packages=packages,
+setup(
+    author="Marco Masetti",
+    author_email='marco.masetti@sky.uk',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="A chaostoolkit control to store events on different data stores/monitoring platfoms",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
-    install_requires=install_require,
-    tests_require=test_require,
-    setup_requires=pytest_runner,
-    python_requires='>=3.5.*'
+    keywords='chaostoolkit-storeevent',
+    name='chaostoolkit-storeevent',
+    packages=find_packages(include=['chaostoolkit-storeevent']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/grubert65/chaostoolkit-storeevent',
+    version='0.0.9',
+    zip_safe=False,
 )
-
-
-def main():
-    """Package installation entry point."""
-    setuptools.setup(**setup_params)
-
-if __name__ == '__main__':
-    main()
