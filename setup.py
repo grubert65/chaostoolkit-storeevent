23c1e845ddff16ed7ace8703e7ea2ddb0edec2dd
Masetti, Marco <marco.masetti@sky.uk>
2022-03-21 17:20:16 +0000

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,60 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """The setup script."""
 
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-requirements = []
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
-
-
-def get_version_from_package() -> str:
-    path = os.path.join(os.path.dirname(__file__), "chaosdb/__init__.py")
-    path = os.path.normpath(os.path.abspath(path))
-    with open(path) as f:
-        for line in f:
-            if line.startswith("__version__"):
-                token, version = line.split(" = ", 1)
-                version = version.replace("'", "").strip()
-                return version
-
-
-setup(
-    author="Marco Masetti",
-    author_email='marco.masetti@sky.uk',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="A chaostoolkit control \
- to store events on different data stores/monitoring platfoms",
-    install_requires=requirements,
-    license="BSD license",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    include_package_data=True,
-    keywords='chaosdb',
-    name='chaosdb',
-    packages=find_packages(include=['chaosdb']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/sky-uk/chaostoolkit-storeevent',
-    version=get_version_from_package(),
-    zip_safe=False,
-)
+setup()
